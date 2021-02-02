49d53cec1df767b1e9443b0a8f0d5d348e0eeedb
Dominik Białasiewicz <d.bialasiewicz@successup.pl>
2021-02-02 19:58:42 +0100

    Register company with new user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\LoginFormAuthenticator;
@@ -11,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
 
 class RegistrationController extends AbstractController
 {
@@ -22,10 +24,17 @@ class RegistrationController extends AbstractController
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-            dd($request->request->get('registration_form')['name']);
+
+            $company = new Company();
+            $company->setEmail($request->request->get('registration_form')['email']);
+            $company->setName($request->request->get('registration_form')['name']);
+            $company->setNIP($request->request->get('registration_form')['NIP']);
+            $company->setPhone($request->request->get('registration_form')['phone']);
+            $company->setStatus(0);
+            $user->setCompany($company);
+            $user->setRoles(array('ROLE_ADMIN'));
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
@@ -34,6 +43,7 @@ class RegistrationController extends AbstractController
             );
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
+            $entityManager->persist($company);
             $entityManager->flush();
             // do anything else you need here, like send an email
 
